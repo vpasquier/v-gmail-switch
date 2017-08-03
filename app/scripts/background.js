1f7d26fb5cec818d0b7f1455f581844620689b08
Vladimir Pasquier <vpasquier@nuxeo.com>
2017-08-03 19:18:25 -0400

    replace Verdict by V

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- Copyright ~ Verdict
+ Copyright ~ V
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
--- app/scripts/chromereload.js
+++ app/scripts/chromereload.js
@@ -1,5 +1,5 @@
 /*
-Copyright ~ Verdict
+Copyright ~ V
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
